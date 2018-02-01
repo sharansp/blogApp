043117a072f3f4d0ead75840ebf5bc32c73802b4
sharan <sharangowda2425@gmail.com>
2018-02-01 12:04:54 +0530

    initial Commits

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import ChatBot from 'react-simple-chatbot';
+//import './index.css';
+
+class Bot extends React.Component{
+  /*constructor(props){
+    super();
+  }*/
+  render(){
+    return(
+    <h1>Header</h1>
+    )
+
+  }
+    
+
+}
+
+
+ReactDOM.render(
+  <Bot />,
+  document.getElementById('root')
+); 
